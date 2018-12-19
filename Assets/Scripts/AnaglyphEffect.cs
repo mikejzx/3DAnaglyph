fbb4ca3c503b79289929e67b626a7f4073ce36a9
Michael <mikeskec2@gmail.com>
2018-12-19 17:08:23 +1100

    initial commit

---
+++ Assets/Scripts/AnaglyphEffect.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Michael's 3-D Anaglyph effect.
+    Originally written 16.10.2018
+*/
+
+[ExecuteInEditMode]
+public class AnaglyphEffect : MonoBehaviour {
+
+    public Shader fxShader;
+    public Camera cam2;
+    public float stereoWidth = 1.0f;
+    public float eyeWidth = 0.2f; // Used for experimental purposes.
+
+    private Material mat;
+    private RenderTexture rt;
+
+    private void Start()
+    {
+        // Adjust camera y-angles based on stereo width.
+        transform.localEulerAngles = Vector3.up * stereoWidth;
+        cam2.transform.localEulerAngles = Vector3.up * -stereoWidth;
+
+        // Distance between eyes
+        transform.localPosition = Vector3.right * eyeWidth;
+        cam2.transform.localPosition = Vector3.right * -eyeWidth;
+    }
+
+    private void OnEnable()
+    {
+        // Prevent errors.
+        int w = Screen.width, h = Screen.height;
+        if (fxShader == null || w == 0 || h == 0) {
+            enabled = false;
+            return;
+        }
+
+        // Initialise materials used for blitting.
+        mat = new Material(fxShader);
+        mat.hideFlags = HideFlags.HideAndDontSave;
+        cam2.enabled = false;
+
+        // Initialse render texture.
+        rt = new RenderTexture(w, h, 8, RenderTextureFormat.Default);
+        cam2.targetTexture = rt;
+    }
+
+    private void OnDisable()
+    {
+        // Clean up resources.
+        if (mat != null) { DestroyImmediate(mat); }
+        if (rt != null) { rt.Release(); }
+        cam2.targetTexture = null;
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (cam2 == null || mat == null || rt == null) {
+            enabled = false;
+            return;
+        }
+
+        // Render to render texture
+        cam2.Render();
+
+        // Apply second texture to shader. ("_MainTex" is automatically applied by Unity3D)
+        mat.SetTexture("_MainTex2", rt);
+
+        // Blit !
+        Graphics.Blit(source, destination, mat);
+
+        // Clean up RenderTexture resources. (Not sure if this is required???)
+        rt.Release();
+    }
+}
---
+++ Assets/Shaders/AnaglyphShader.shader
@@ -0,0 +1,61 @@
+﻿Shader "Hidden/AnaglyphShader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_MainTex2("2nd Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Cull Off 
+		ZWrite Off 
+		ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			uniform sampler2D _MainTex;
+			uniform sampler2D _MainTex2;
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+
+				return o;
+			}
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 sideA = tex2D(_MainTex, i.uv);
+				fixed4 sideB = tex2D(_MainTex2, i.uv);
+
+				fixed3 red = fixed3(sideA.r, 0.0, 0.0);
+				fixed3 cyan = fixed3(0.0, sideB.gb);
+
+				fixed4 col = fixed4(red + cyan, sideA.a);
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
